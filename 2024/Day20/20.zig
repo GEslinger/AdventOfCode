356f4f4c4397569151aae849d304650b1baf6cd8
GEslinger <30093287+GEslinger@users.noreply.github.com>
2025-08-27 21:23:28 -0400

    no real progress, but optimizing

--- 2024/Day20/20.zig
+++ 2024/Day20/20.zig
@@ -61,38 +61,51 @@ pub fn main() !void {
     var cheats = std.AutoHashMap(@Vector(4, usize), u64).init(alloc);
     defer cheats.deinit();
     var c_tree: std.ArrayList(Node) = .empty;
-    var c_tree_seen = std.AutoHashMap(@Vector(2, usize), void).init(alloc);
+    var c_tree_seen = std.AutoHashMap(@Vector(2, usize), u64).init(alloc);
     defer c_tree.deinit(alloc);
 
     var path_iter = path.iterator();
     while (path_iter.next()) |node| {
         const t_start = node.value_ptr.*;
-        const pos_start = node.key_ptr.*; //FIXME: The start position is actually the first step into the wall!
+        const pos_start = node.key_ptr.*;
         print("Starting {}\n", .{pos_start});
-        try c_tree.append(alloc, Node{ .pos = pos_start, .len = 1 });
+
+        for (neighbors(map, pos_start)) |n_opt| {
+            if (n_opt) |n| {
+                if (map[n[0]][n[1]] == '#') {
+                    try c_tree.append(alloc, Node{ .pos = n, .len = 1 });
+                }
+            }
+        }
 
         while (c_tree.pop()) |c_node| {
             //print("Popped investigating {}\n", .{c_node.pos});
-            if (c_node.len >= 20) continue;
+            if (c_node.len > 20) continue;
+
+            if (map[c_node.pos[0]][c_node.pos[1]] != '#') {
+                const t_end = path.get(c_node.pos).?;
+                if (t_start + c_node.len >= t_end) continue;
+                const savings = t_end - (t_start + c_node.len);
+                print("Valid cheat starting {} ending at {} saving {}\n", .{ pos_start, c_node.pos, savings });
+                const entry = try cheats.getOrPut(@Vector(4, usize){ pos_start[0], pos_start[1], c_node.pos[0], c_node.pos[1] });
+                if (entry.found_existing) {
+                    //print("It's already there, updating savings\n", .{});
+                    if (entry.value_ptr.* < savings) entry.value_ptr.* = savings;
+                } else {
+                    entry.value_ptr.* = savings;
+                }
+
+                continue;
+            }
+
             for (neighbors(map, c_node.pos)) |n_opt| {
                 if (n_opt) |n| {
-                    if (c_tree_seen.contains(n)) continue;
-                    if (map[n[0]][n[1]] == '#') {
-                        //print("Gonna add neighbor {}\n", .{n});
-                        try c_tree.append(alloc, Node{ .pos = n, .len = c_node.len + 1 });
-                        try c_tree_seen.put(n, {});
-                    } else {
-                        const t_end = path.get(n).?;
-                        if (t_start + c_node.len >= t_end) continue;
-                        const savings = t_end - (t_start + c_node.len);
-                        print("Valid cheat starting {} ending at {} saving {}\n", .{ pos_start, n, savings });
-                        const entry = try cheats.getOrPut(@Vector(4, usize){ pos_start[0], pos_start[1], n[0], n[1] });
-                        if (entry.found_existing) {
-                            if (entry.value_ptr.* < savings) entry.value_ptr.* = savings;
-                        } else {
-                            entry.value_ptr.* = savings;
-                        }
+                    if (c_tree_seen.contains(n)) {
+                        if (c_tree_seen.get(n).? < c_node.len + 1) continue;
                     }
+                    //print("Gonna add neighbor {}\n", .{n});
+                    try c_tree.append(alloc, Node{ .pos = n, .len = c_node.len + 1 });
+                    try c_tree_seen.put(n, c_node.len + 1);
                 }
             }
         }
