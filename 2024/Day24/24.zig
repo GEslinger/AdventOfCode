cae61c0bf26fec2d919f08c5201ee939790a808b
GEslinger <30093287+GEslinger@users.noreply.github.com>
2025-09-07 21:47:37 -0400

    progress on solving 24, checking bit pairs

--- 2024/Day24/24.zig
+++ 2024/Day24/24.zig
@@ -62,60 +62,41 @@ pub fn main() !void {
     print("\n", .{});
     for (ops.items) |op| print("{s} {any} {s} -> {s}\n", .{ op.a, op.op, op.b, op.c });
 
-    var done = false;
-    while (!done) {
-        for (ops.items) |op| {
-            if (vals.get(op.a).? == null or vals.get(op.b).? == null) continue;
-            const a = vals.get(op.a).?.?;
-            const b = vals.get(op.b).?.?;
+    for (0..45) |input_bit| {
+        var try_vals = try vals.clone();
 
-            const c_value = vals.getEntry(op.c).?.value_ptr;
-            c_value.* = switch (op.op) {
-                .XOR => a ^ b,
-                .AND => a & b,
-                .OR => a | b,
-            };
+        for (0..45) |reset_bit| {
+            var buf: [3]u8 = undefined;
+            var reset = try std.fmt.bufPrint(&buf, "y{d:0>2}", .{reset_bit});
+            if (try_vals.getPtr(buf)) |val_ptr| val_ptr.* = 0;
+            reset = try std.fmt.bufPrint(&buf, "x{d:0>2}", .{reset_bit});
+            if (try_vals.getPtr(buf)) |val_ptr| val_ptr.* = 0;
         }
 
-        // Check if all z.. gates are not null
-        var check_done_iter = vals.iterator();
-        done = while (check_done_iter.next()) |entry| {
-            if (entry.key_ptr.*[0] == 'z' and entry.value_ptr.* == null) break false;
-        } else true;
-    }
+        _ = input_bit;
 
-    val_iter = vals.iterator();
+        var done = false;
+        while (!done) {
+            for (ops.items) |op| {
+                if (try_vals.get(op.a).? == null or try_vals.get(op.b).? == null) continue;
+                const a = try_vals.get(op.a).?.?;
+                const b = try_vals.get(op.b).?.?;
 
-    // Delete all non-z
-    while (val_iter.next()) |entry| {
-        if (entry.key_ptr.*[0] != 'z') {
-            if (vals.swapRemove(entry.key_ptr.*)) val_iter = vals.iterator();
+                const c_value = try_vals.getEntry(op.c).?.value_ptr;
+                c_value.* = switch (op.op) {
+                    .XOR => a ^ b,
+                    .AND => a & b,
+                    .OR => a | b,
+                };
+            }
+
+            // Check if all z.. gates are not null
+            var check_done_iter = try_vals.iterator();
+            done = while (check_done_iter.next()) |entry| {
+                if (entry.key_ptr.*[0] == 'z' and entry.value_ptr.* == null) break false;
+            } else true;
         }
     }
-
-    // Sort function
-    const C = struct {
-        keys: [][3]u8,
-
-        pub fn lessThan(ctx: @This(), a_index: usize, b_index: usize) bool {
-            const val_a = std.fmt.parseInt(u8, ctx.keys[a_index][1..3], 10) catch 0;
-            const val_b = std.fmt.parseInt(u8, ctx.keys[b_index][1..3], 10) catch 0;
-            return val_a > val_b;
-        }
-    };
-
-    vals.sort(C{ .keys = vals.keys() });
-
-    // Run through sorted list and construct final number
-    val_iter = vals.iterator();
-    var num: u64 = 0;
-    while (val_iter.next()) |entry| {
-        print("{s}: {any}\n", .{ entry.key_ptr.*, entry.value_ptr.*.? });
-        num <<= 1;
-        num |= entry.value_ptr.*.?;
-    }
-
-    print("Final: {}\n", .{num});
 }
 
 const Operation = struct {
